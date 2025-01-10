e767f0053a46e47479d22556826a35d6d2cbb8a6
enrique047 <enriquecrasto1@gmail.com>
2025-01-10 11:42:09 +0530

    Added Easy folder

---
+++ Easy/1408.java
@@ -0,0 +1,20 @@
+//String Matching in an Array
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        int n = words.length;
+        Set<String> st = new HashSet<>();
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+
+                if(i != j && words[i].contains(words[j])){
+                    if(!st.contains(words[j])){
+                        st.add(words[j]);
+                    }
+                }
+            }
+        }
+
+        return new ArrayList<>(st);
+    }
+}
