9c1da6c2ef2bb942bec8ef25525a7f4ca0584691
enrique047 <enriquecrasto1@gmail.com>
2025-10-26 17:12:50 +0530

    Added M folder

---
+++ Medium/2043.java
@@ -0,0 +1,50 @@
+//Simple Bank System
+class Bank {
+    long[] a;
+    int n ;
+
+    public Bank(long[] balance) {
+        a = balance;
+        n = a.length;
+    }
+    
+    public boolean transfer(int account1, int account2, long money) {
+
+        if(account1 - 1 >= n || account2 - 1 >= n || a[account1-1] < money){
+            return false;
+        }
+
+        a[account1-1] -= money;
+        a[account2-1] += money;
+
+        return true;
+    }
+    
+    public boolean deposit(int account, long money) {
+        if(account - 1 >= n){
+            return false;
+        }
+        
+        a[account-1] += money;
+
+        return true;
+    }
+    
+    public boolean withdraw(int account, long money) {
+        if(account - 1 >= n || a[account - 1] < money){
+            return false;
+        }
+        
+        a[account-1] -= money;
+
+        return true;
+    }
+}
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank obj = new Bank(balance);
+ * boolean param_1 = obj.transfer(account1,account2,money);
+ * boolean param_2 = obj.deposit(account,money);
+ * boolean param_3 = obj.withdraw(account,money);
+ */
