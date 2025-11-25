05391f0c745fe1da89b1ccb1d9cd6c73c3915b45
enrique047 <enriquecrasto1@gmail.com>
2025-11-25 22:39:53 +0530

    Added M folder

---
+++ Medium/1015.java
@@ -0,0 +1,17 @@
+//Smallest Integer Divisible by K
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        int remainder = 0;
+
+        for(int length = 1 ; length <= k; length++){
+            
+            remainder = (remainder * 10 + 1) % k;
+
+            if(remainder == 0){
+                return length;
+            }
+        }
+
+        return -1;
+    }
+}
