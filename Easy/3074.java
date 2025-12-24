a3fdd35a17e6d38daee098a125fe9615f151f1c0
enrique047 <enriquecrasto1@gmail.com>
2025-12-24 20:03:31 +0530

    Added E folder

---
+++ Easy/3074.java
@@ -0,0 +1,24 @@
+//Apple Redistribution into Boxes
+class Solution {
+    public int minimumBoxes(int[] apple, int[] capacity) {
+        int n = capacity.length;
+        Arrays.sort(capacity);
+
+        int appleSum = 0;
+
+        for(int i : apple){
+            appleSum += i;
+        }
+
+        int res = 0;
+
+        for(int i = n - 1 ; i >= 0 ; i--){
+            appleSum -= capacity[i];
+            res++;
+
+            if(appleSum <= 0) return res;
+        }
+
+        return res;
+    }
+}
